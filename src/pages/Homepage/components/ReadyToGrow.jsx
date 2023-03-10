be9a67fb825485692d255eb13feae002c6dd0262
maksimvasilevskiy <97239271+maksimvasilevskiy@users.noreply.github.com>
2023-03-10 12:54:30 +0300

    extra elements removed

--- src/pages/Homepage/components/ReadyToGrow.jsx
+++ src/pages/Homepage/components/ReadyToGrow.jsx
@@ -4,69 +4,12 @@ import {
   Container,
   Flex,
   Heading,
-  Icon,
-  Stack,
   Text,
-  useColorModeValue,
   Image
 } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 
 import ready_to_grow_bg from '../../../images/ready-to-grow-bg.svg';
-import arrow_right from '../../../images/arrow-right.svg';
-
-const Card = ({ heading, description, icon, href }) => {
-  return (
-    <Box
-      maxW={"full"}
-      w={'full'}
-      borderWidth="1px"
-      borderRadius="20px"
-      borderColor="#ECECEC"
-      overflow="hidden"
-      p={{ base: '24px 16px', md: '46px' }}>
-      <Box align={{ base: 'left', md: 'center' }}>
-        <Image w={{ base: '45px', md: '60px' }} h={{ base: '45px', md: '60px' }} src={icon} alt="" />
-        <Box mt={{ base: '12px', md: '24px' }}>
-          <Heading
-            fontWeight={500}
-            textTransform={"uppercase"}
-            fontSize={{ base: '18px', md: '20px' }}>
-            {heading}
-          </Heading>
-          <Text
-            maxW={"361px"}
-            mt={{ base: '4px', md: '8px' }}
-            fontSize={{ base: '14px', md: '16px' }}
-            lineHeight={'22px'}
-            fontWeight={500}>
-            {description}
-          </Text>
-        </Box>
-        <Button
-          border={'1.5px solid #4838E4'}
-          borderRadius={'12px'}
-          p={{ base: '5px 24px', md: '7px 24px' }}
-          mt={{ base: '56px', md: '40px' }}
-          variant={'link'}
-          color={'#4838E4'}
-          size={'sm'}
-          fontSize={"16px"}
-          fontWeight={600}
-          _hover={{
-            textDecoration: 'none',
-        }}>
-          Learn more
-          <Image
-            src={arrow_right}
-            alt=""
-            ml={'8px'}
-          />
-        </Button>
-      </Box>
-    </Box>
-  );
-};
 
 const ReadyToGrow = () => {
   return (
